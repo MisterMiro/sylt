0ccdc356e0401550a5d10c542c8428e518444d1a
MisterMiro <mrhag456@gmail.com>
2024-07-15 05:45:21 +0200

    Updated documentation and build script

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+
+echo building release...
 gcc src/sylt.c -o bin/sylt_0.1 -Wall -Wextra -Werror -std=c99 -O3 -lm -D_POSIX_C_SOURCE=199309L -DRELEASE
+
+echo building debug...
 gcc src/sylt.c -o bin/sylt_0.1_debug -Wall -Wextra -Werror -std=c99 -lm -D_POSIX_C_SOURCE=199309L -DDEBUG

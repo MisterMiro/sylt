027fa3908d107e331c9a52b000a52d1d2e8d8dd4
Miro <158522045+MisterMiro@users.noreply.github.com>
2024-09-19 20:16:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 
 echo building release...
 gcc src/sylt.c -o sylt -Wall -Wextra -std=c99 -O3 -lm -D_POSIX_C_SOURCE=199309L -DRELEASE
+
+echo building debug...
+gcc src/sylt.c -o sylt_dbg -Wall -Wextra -std=c99 -O3 -lm -D_POSIX_C_SOURCE=199309L -DDEBUG

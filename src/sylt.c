cde45df6eccd20aa7f9a912de016f013dc2716c9
Miro <158522045+MisterMiro@users.noreply.github.com>
2024-09-19 20:49:09 +0200

    Update sylt.c

--- src/sylt.c
+++ src/sylt.c
@@ -5503,7 +5503,7 @@ void compile_and_run(sylt_t* ctx, string_t* src, string_t* name, comp_t* cmp) {
 	
 	emit_nullary(cmp, OP_RET);
 	set_state(ctx, SYLT_STATE_COMPILED);
-	puts("hello");
+	
 	/* load program */
 	sylt_pushclosure(ctx, closure_new(ctx, cmp->func));
 	sylt_call(ctx, 0);
